a245a6fb5b4cddec7139d23f9347ad07d9f1f535
Abdelrahman Ahmed <abdelrahman@me>
2019-05-05 21:08:15 +0200

    Create LoginForm design

--- src/GUI/LoginForm.java
+++ src/GUI/LoginForm.java
@@ -14,8 +14,12 @@ public class LoginForm extends javax.swing.JFrame {
     /**
      * Creates new form LoginForm
      */
-    public LoginForm() {
+    
+    public LoginForm() 
+    {
         initComponents();
+        setResizable(false);
+        AdminCheckbox.setSelected(true);
     }
 
     /**
@@ -27,21 +31,237 @@ public class LoginForm extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        buttonGroup1 = new javax.swing.ButtonGroup();
+        jPanel1 = new javax.swing.JPanel();
+        logo = new javax.swing.JLabel();
+        AdminCheckbox = new javax.swing.JCheckBox();
+        Officer = new javax.swing.JCheckBox();
+        CardLayoutPanel = new javax.swing.JPanel();
+        AdminPanel = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jSeparator1 = new javax.swing.JSeparator();
+        UsernameTextfield = new javax.swing.JTextField();
+        jSeparator2 = new javax.swing.JSeparator();
+        PasswordTextfield = new javax.swing.JPasswordField();
+        OfficerPanel = new javax.swing.JPanel();
+        jLabel3 = new javax.swing.JLabel();
+        IdTextField = new javax.swing.JTextField();
+        jSeparator3 = new javax.swing.JSeparator();
+        LoginButton = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setAutoRequestFocus(false);
+        setBackground(new java.awt.Color(0, 40, 89));
+
+        jPanel1.setBackground(new java.awt.Color(0, 40, 89));
+
+        logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/GUI/logo.jpg"))); // NOI18N
+
+        AdminCheckbox.setBackground(new java.awt.Color(0, 40, 89));
+        buttonGroup1.add(AdminCheckbox);
+        AdminCheckbox.setFont(new java.awt.Font("Century", 0, 24)); // NOI18N
+        AdminCheckbox.setForeground(new java.awt.Color(0, 120, 187));
+        AdminCheckbox.setText("Admin");
+        AdminCheckbox.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                AdminCheckboxActionPerformed(evt);
+            }
+        });
+
+        Officer.setBackground(new java.awt.Color(0, 40, 89));
+        buttonGroup1.add(Officer);
+        Officer.setFont(new java.awt.Font("Century", 0, 24)); // NOI18N
+        Officer.setForeground(new java.awt.Color(0, 120, 187));
+        Officer.setText("Officer");
+        Officer.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                OfficerActionPerformed(evt);
+            }
+        });
+
+        CardLayoutPanel.setLayout(new java.awt.CardLayout());
+
+        AdminPanel.setBackground(new java.awt.Color(0, 40, 89));
+
+        jLabel1.setFont(new java.awt.Font("Century Gothic", 0, 28)); // NOI18N
+        jLabel1.setForeground(new java.awt.Color(0, 120, 187));
+        jLabel1.setText("Password");
+
+        jLabel2.setFont(new java.awt.Font("Century Gothic", 0, 28)); // NOI18N
+        jLabel2.setForeground(new java.awt.Color(0, 120, 187));
+        jLabel2.setText("Username");
+
+        jSeparator1.setBackground(new java.awt.Color(0, 120, 187));
+        jSeparator1.setForeground(new java.awt.Color(0, 120, 187));
+
+        UsernameTextfield.setBackground(new java.awt.Color(0, 40, 89));
+        UsernameTextfield.setFont(new java.awt.Font("Century Gothic", 0, 26)); // NOI18N
+        UsernameTextfield.setForeground(new java.awt.Color(255, 255, 255));
+        UsernameTextfield.setBorder(null);
+
+        jSeparator2.setBackground(new java.awt.Color(0, 120, 187));
+        jSeparator2.setForeground(new java.awt.Color(0, 120, 187));
+
+        PasswordTextfield.setBackground(new java.awt.Color(0, 40, 89));
+        PasswordTextfield.setFont(new java.awt.Font("Century Gothic", 0, 26)); // NOI18N
+        PasswordTextfield.setForeground(new java.awt.Color(255, 255, 255));
+        PasswordTextfield.setBorder(null);
+
+        javax.swing.GroupLayout AdminPanelLayout = new javax.swing.GroupLayout(AdminPanel);
+        AdminPanel.setLayout(AdminPanelLayout);
+        AdminPanelLayout.setHorizontalGroup(
+            AdminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(AdminPanelLayout.createSequentialGroup()
+                .addGap(32, 32, 32)
+                .addGroup(AdminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jSeparator2, javax.swing.GroupLayout.DEFAULT_SIZE, 342, Short.MAX_VALUE)
+                    .addComponent(UsernameTextfield)
+                    .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jSeparator1)
+                    .addComponent(PasswordTextfield))
+                .addContainerGap(21, Short.MAX_VALUE))
+        );
+        AdminPanelLayout.setVerticalGroup(
+            AdminPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, AdminPanelLayout.createSequentialGroup()
+                .addGap(24, 24, 24)
+                .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(UsernameTextfield)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator1, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(PasswordTextfield, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jSeparator2, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(23, 23, 23))
+        );
+
+        CardLayoutPanel.add(AdminPanel, "card2");
+
+        OfficerPanel.setBackground(new java.awt.Color(0, 40, 89));
+
+        jLabel3.setFont(new java.awt.Font("Century Gothic", 0, 28)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(0, 120, 187));
+        jLabel3.setText("ID");
+
+        IdTextField.setBackground(new java.awt.Color(0, 40, 89));
+        IdTextField.setFont(new java.awt.Font("Century Gothic", 0, 26)); // NOI18N
+        IdTextField.setForeground(new java.awt.Color(255, 255, 255));
+        IdTextField.setBorder(null);
+        IdTextField.setMaximumSize(new java.awt.Dimension(0, 33));
+
+        jSeparator3.setBackground(new java.awt.Color(0, 120, 187));
+        jSeparator3.setForeground(new java.awt.Color(0, 120, 187));
+
+        javax.swing.GroupLayout OfficerPanelLayout = new javax.swing.GroupLayout(OfficerPanel);
+        OfficerPanel.setLayout(OfficerPanelLayout);
+        OfficerPanelLayout.setHorizontalGroup(
+            OfficerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(OfficerPanelLayout.createSequentialGroup()
+                .addGap(26, 26, 26)
+                .addGroup(OfficerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(IdTextField, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 163, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 342, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(27, Short.MAX_VALUE))
+        );
+        OfficerPanelLayout.setVerticalGroup(
+            OfficerPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(OfficerPanelLayout.createSequentialGroup()
+                .addGap(51, 51, 51)
+                .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 45, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(IdTextField, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jSeparator3, javax.swing.GroupLayout.PREFERRED_SIZE, 10, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(46, 46, 46))
+        );
+
+        CardLayoutPanel.add(OfficerPanel, "card3");
+
+        LoginButton.setBackground(new java.awt.Color(0, 59, 115));
+        LoginButton.setFont(new java.awt.Font("Century Gothic", 0, 24)); // NOI18N
+        LoginButton.setForeground(new java.awt.Color(255, 255, 255));
+        LoginButton.setText("Log In");
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(AdminCheckbox)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(Officer)
+                .addGap(146, 146, 146))
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(97, 97, 97)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(LoginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 290, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(logo))
+                .addContainerGap(105, Short.MAX_VALUE))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(53, 53, 53)
+                    .addComponent(CardLayoutPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 395, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(54, Short.MAX_VALUE)))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(logo)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(AdminCheckbox)
+                    .addComponent(Officer))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 356, Short.MAX_VALUE)
+                .addComponent(LoginButton, javax.swing.GroupLayout.PREFERRED_SIZE, 51, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(37, 37, 37))
+            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel1Layout.createSequentialGroup()
+                    .addGap(402, 402, 402)
+                    .addComponent(CardLayoutPanel, javax.swing.GroupLayout.PREFERRED_SIZE, 258, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addContainerGap(132, Short.MAX_VALUE)))
+        );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void AdminCheckboxActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_AdminCheckboxActionPerformed
+        CardLayoutPanel.removeAll();
+        CardLayoutPanel.add(AdminPanel);
+        CardLayoutPanel.repaint();
+        CardLayoutPanel.revalidate();
+        UsernameTextfield.setText("");
+        PasswordTextfield.setText("");
+        
+    }//GEN-LAST:event_AdminCheckboxActionPerformed
+
+    private void OfficerActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_OfficerActionPerformed
+        CardLayoutPanel.removeAll();
+        CardLayoutPanel.add(OfficerPanel);
+        CardLayoutPanel.repaint();
+        CardLayoutPanel.revalidate();
+        IdTextField.setText("");
+    }//GEN-LAST:event_OfficerActionPerformed
 
     /**
      * @param args the command line arguments
@@ -79,5 +299,23 @@ public class LoginForm extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JCheckBox AdminCheckbox;
+    private javax.swing.JPanel AdminPanel;
+    private javax.swing.JPanel CardLayoutPanel;
+    private javax.swing.JTextField IdTextField;
+    private javax.swing.JButton LoginButton;
+    private javax.swing.JCheckBox Officer;
+    private javax.swing.JPanel OfficerPanel;
+    private javax.swing.JPasswordField PasswordTextfield;
+    private javax.swing.JTextField UsernameTextfield;
+    private javax.swing.ButtonGroup buttonGroup1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JSeparator jSeparator1;
+    private javax.swing.JSeparator jSeparator2;
+    private javax.swing.JSeparator jSeparator3;
+    private javax.swing.JLabel logo;
     // End of variables declaration//GEN-END:variables
 }
--- src/crime/management/system/Main.java
+++ src/crime/management/system/Main.java
@@ -1,9 +1,13 @@
 package crime.management.system;
-
+import GUI.AdminForm;
+import GUI.LoginForm;
+import javax.swing.*;
 public class Main
 {
     public static void main(String[] args)
     {
         System.out.println("3albaraka");
+        JFrame frame = new LoginForm();
+        frame.show();
     }
 }
